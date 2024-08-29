f8a715a1af0aa562bfd2e23766d9eea6374aa5fa
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-29 13:44:56 +0200

    snack1

--- js/snack1.js
+++ js/snack1.js
@@ -13,3 +13,34 @@ Generiamo e stampiamo in console la lista per i segnaposto.
 */
 
 
+// Creo una costante per il nome del tavolo
+const tableName = 'Tavolo Vip';
+
+// Creo un array con i nomi degli ospiti
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'George Clooney',
+    'Amal Clooney',
+    'Fedez',
+    'Amadeus',
+    'Fiorello'
+];
+
+// Creo la lista di tutti gli ospiti
+const guestList = guests.map((guestName, i) => {
+    return {
+        tableName: tableName,
+        guestName: guestName,
+
+        // Posizione posto assegnata
+        place: i + 1
+    };
+});
+
+// Stampo in console
+console.log(guestList);
