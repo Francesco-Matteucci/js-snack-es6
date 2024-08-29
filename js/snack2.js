dadad6a8d27ccd2bea2f31dac4b37ee48d2682ef
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-29 13:54:45 +0200

    Snack2

--- js/snack2.js
+++ js/snack2.js
@@ -16,3 +16,26 @@ Id  Name                Grades
 120 Francesca da Polenta    84
 
 */
+
+//Creo un array di oggetti per gli studenti
+const students = [
+    { id: 213, name: 'Marco della Rovere', grades: 78 },
+    { id: 110, name: 'Paola Cortellessa', grades: 96 },
+    { id: 250, name: 'Andrea Mantegna', grades: 48 },
+    { id: 145, name: 'Gaia Borromini', grades: 74 },
+    { id: 196, name: 'Luigi Grimaldello', grades: 68 },
+    { id: 102, name: 'Piero della Francesca', grades: 50 },
+    { id: 120, name: 'Francesca da Polenta', grades: 84 }
+];
+
+// Utilizzo il map, per trasformare i nomi degli studenti in maiuscolo 
+const upperCaseNames = students.map(student => student.name.toUpperCase());
+console.log(upperCaseNames);
+
+// Attraverso filter trovo tutti gli studenti con media-voto superiore a 70
+const excellentStudents = students.filter(student => student.grades > 70);
+console.log(excellentStudents);
+
+// Uso ancora una volta filter per stabilire quali studenti hanno sia la media-voto superiore a 70 che l'ID superiore a 120
+const qualifiedStudents = students.filter(student => student.grades > 70 && student.id > 120);
+console.log(qualifiedStudents);
